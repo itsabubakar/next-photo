0d648090b24fa423ac72d901fb3bcaafadc4ebdc
Sadiq <seeker99157@gmial.com>
2022-09-18 12:59:47 +0100

    finished site

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -5,14 +5,14 @@ import IgImg3 from '../public/ig-img-3.jpeg';
 import IgImg4 from '../public/ig-img-4.jpeg';
 import IgImg5 from '../public/ig-img-5.jpeg';
 import IgImg6 from '../public/ig-img-6.jpeg';
-import InstagramImg from './IgImg';
+import InstagramImg from './InstagramImg';
 
 
 const Instagram = () => {
   return (
     <div className='max-w-[1240px] mx-auto text-center py-24'>
         <p className='text-2xl font-bold'>Follow me on Instagram</p>
-        <p className='pb-4'>@Next{'>'}</p>
+        <p className='pb-4'>@Next &gt;</p>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
             <InstagramImg socialImg={IgImg1} />
             <InstagramImg socialImg={IgImg2} />
---
+++ pages/contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Contact from '../components/Contact'
+
+const contact = () => {
+  return (
+    <div>
+      <Hero  heading='Contact' message='Contact for more work and qoutes'/>
+      <Contact />
+    </div>
+  )
+}
+
+export default contact
